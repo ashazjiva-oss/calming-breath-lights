f21836af988bb88073ad119a18d1d7960250b4f4
ashazjiva-oss <ashazjiva@gmail.com>
2026-06-09 18:32:19 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,18 +7,19 @@ input.onButtonPressed(Button.AB, function () {
         . . . . .
         . . . . .
         `)
-    basic.pause(500)
+    basic.pause(2000)
     basic.showIcon(IconNames.SmallDiamond)
-    basic.pause(500)
+    basic.pause(2000)
     basic.showIcon(IconNames.Diamond)
     basic.pause(2000)
     basic.showIcon(IconNames.SmallDiamond)
-    basic.pause(500)
+    basic.pause(5000)
     basic.showLeds(`
         . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
         `)
+    basic.clearScreen()
 })
